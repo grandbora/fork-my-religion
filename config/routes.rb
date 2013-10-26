1b6102c90fdc931c619421007adc8f6cb0de0b7f
grandbora <bora@soundcloud.com>
2013-10-26 19:01:53 +0200

    dist

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ ForkMyReligion::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'discover#index'
+  # root 'discover#index'
+  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
